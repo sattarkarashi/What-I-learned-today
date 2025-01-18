e280b638fe9803955b5d7208a66dc824efc76fe2
sattarkarashi <satokarashi@gmail.com>
2025-01-18 09:23:30 +0100

    Explored the inner working of dunder methods

---
+++ fluent_python/dunder_methods.py
@@ -0,0 +1,31 @@
+import collections
+from random import choice
+"""
+Dunder methods are special methods that are called by Python when you use certain syntax.
+They are always surrounded by double underscores, hence the name.
+"""
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+    
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+    
+    def __len__(self):
+        return len(self._cards)
+    
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+# Add sorting to the deck
+def spades_high(card):
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    print()
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+deck = FrenchDeck()
+print(sorted(deck, key=spades_high))
