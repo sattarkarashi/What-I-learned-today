f00170ec4fccee864a0251e3ba8a203236398b0e
sattarkarashi <satokarashi@gmail.com>
2025-02-01 21:10:27 +0100

    Setdefault performance in dictionary instead of dict.get() or __getitem__()

---
+++ fluent_python/dictionaries/dictionaries.py
@@ -0,0 +1,37 @@
+# Handling missing keys with setdefault
+
+# An unefficient way to handle missing keys in a dictionary is to use the get method as shown below:
+import sys
+import re
+
+WORD_RE = re.compile(r'\w+') # \w+ matches one or more word characters
+index = {}
+with open(sys.argv[1], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        print(line_no, line)
+        for match in WORD_RE.finditer(line):
+            
+            word = match.group()
+            print(match, word)
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            # this is ugly; coded like this to make a point
+            occurrences = index.get(word, [])
+            occurrences.append(location)
+
+            index[word] = occurrences
+
+print(index)
+
+# The above code can be written in a more efficient way using the setdefault method as shown below:
+index = {}
+with open(sys.argv[1], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            index.setdefault(word, []).append(location)
+
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
