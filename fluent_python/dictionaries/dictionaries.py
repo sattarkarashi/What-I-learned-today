91fb4de74405f46b93e9ab7ba9dceab0a800ab0c
sattarkarashi <satokarashi@gmail.com>
2025-02-02 20:17:43 +0100

    using defaultdict as an alternative for dicts

--- fluent_python/dictionaries/dictionaries.py
+++ fluent_python/dictionaries/dictionaries.py
@@ -35,3 +35,22 @@ with open(sys.argv[1], encoding='utf-8') as fp:
 
 for word in sorted(index, key=str.upper):
     print(word, index[word])
+
+
+print("=====================================")
+# Using defaultdict to handle missing keys
+from collections import defaultdict
+
+WORD_RE = re.compile(r'\w+')
+index = defaultdict(list)
+
+with open(sys.argv[1], encoding='utf-8') as fp:
+    for line_no, line in enumerate(fp, 1):
+        for match in WORD_RE.finditer(line):
+            # print(index)
+            word = match.group()
+            column_no = match.start() + 1
+            location = (line_no, column_no)
+            index[word].append(location)
+for word in sorted(index, key=str.upper):
+    print(word, index[word])
